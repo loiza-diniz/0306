6d712d06aa497504a87aadc06db897cf9b5de75d
luiza <d.luiza@aluno.ifsp.edu.br>
2025-06-03 16:47:08 -0300

    dez pras cinco

--- routes/usuario.py
+++ routes/usuario.py
@@ -7,5 +7,18 @@ usuarios_bp = Blueprint("usuarios", __name__)
 def criar():
     dados_body = request.json
     novo_usuario = criar_usuario(dados_body)
-    return jsonify(novo_usuario.to_dict(    )), 201
+    return jsonify(novo_usuario.to_dict()), 201
+
+@usuarios_bp.route("/usuarios", methods=["GET"])
+def lista_usuarios():
+    lista = listar_usuarios()
+    return jsonify(lista)
+
+@usuarios_bp.route("/usuarios/<int:id>", methods=["GET"])
+def buscar_usuario(id):
+    usuario = lista_um_usuario(id)
+    if usuario:
+        return jsonify(usuario)
+    else:
+        return jsonify({"mensagem": "Usuário não encontrado"}), 404
      
--- services/usuarios_services.py
+++ services/usuarios_services.py
@@ -15,3 +15,17 @@ def criar_usuario(dados):
     novo_usuario = Usuario(gerar_id(), dados["nome"], dados["email"], dados["senha"])
     usuarios.append(novo_usuario)
     return novo_usuario
+
+def listar_usuarios():
+    # lista = []
+    #for u in usuarios:
+        #lista.append(u.to_dict())
+    #return lista
+    lista = [u.to_dict() for u in usuarios]
+    return lista
+
+def lista_um_usuario(id):
+    for u in usuarios:
+        if u.id == id:
+            return u.to_dict()
+    return None
